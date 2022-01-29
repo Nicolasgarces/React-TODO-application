8960f147909588498b22dde37c48e38edf4c5ca9
Nicolas Garces <nicolasgarces0@gmail.com>
2022-01-29 05:00:01 +0000

    terminar actividad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("");
+	const [lista, setLista] = useState([]);
+
+	const guardarDatos = (e) => {
+		e.preventDefault();
+		// console.log("guardando datos");
+		setLista([...lista, tarea]);
+	};
+	console.log(lista);
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<form onSubmit={guardarDatos}>
+				<div className="form-group">
+					<label htmlFor="exampleInputEmail1">Email address</label>
+					<input
+						type="text"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						placeholder="Enter email"
+						onChange={(e) => setTarea(e.target.value)}
+						value={tarea}
+					/>
+					<small id="emailHelp" className="form-text text-muted">
+						We'll never share your email with anyone else.
+					</small>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+			<ul>
+				<ol>
+					{setLista.map((lista) => (
+						<li key={lista}>{lista}</li>
+					))}
+				</ol>
+			</ul>
 		</div>
 	);
 };
