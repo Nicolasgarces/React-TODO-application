43ff0d332705fae1968abf8cc1b46891b868c6df
Nicolas Garces <nicolasgarces0@gmail.com>
2022-02-04 05:10:20 +0000

    se agregó la actividad completa

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	const [tarea, setTarea] = useState("");
 	// lista: guarda el arrary
@@ -12,33 +8,77 @@ const Home = () => {
 
 	const guardarDatos = (e) => {
 		e.preventDefault();
-		// console.log("guardando datos");
-		setLista([...lista, tarea]);
+		setLista([
+			...lista,
+			{ id: lista.length, nombre: tarea },
+		]); /*A la funcion setLista encargada de actualizar la variable lista,
+		se le crea un parametro que contiene un array el cual contiene 
+		la variable id que es igual al array por el largo(lista.length) 
+		y la variable nombre que es igual a tarea la cual se crea para poder renderizar en el <li> */
+		setTarea(
+			""
+		); /*a setTarea le damos un parametro con un string vacio, para que luego de escribir
+		una tarea el input quede vacio */
 	};
-	console.log(lista);
+
+	// funcion para borrar
+	const borrarDatos = (id) => {
+		/*a esta funcion le agragmos el parametro id que pertenece al metodo filter*/
+		const listaActualizada = lista.filter(
+			(item) => item.id !== id
+		); /*aquí creamos el metodo filter
+		 que consite en crear una constante llamada listaActualizada que es igual a lista.filter luego se crea un condicional
+		 que tiene como parametro item que es igual a item.id (id es la variable creada anteriormente)
+		 y le decimos que recorra el array y nos filtre todas las funciones diferentes al id ((item)=> item-id !== id)
+		  */
+		setLista(
+			listaActualizada
+		); /*Aqui llamamos la funcion setLista para agregarle como parametro
+		 el array actualizado que es la funcion listaActualizada */
+	};
+	// console.log(lista);
 	return (
-		<div>
+		<div className="container-lg mt-5">
 			<form onSubmit={guardarDatos}>
-				<div className="form-group">
-					<label htmlFor="exampleInputEmail">TODOS</label>
+				{" "}
+				{/*aqui utilizamos el evento onSubmit y dentro de el llamamos la funcion guardarDatos */}
+				<div className="form-group ">
+					<h1 className="d-flex justify-content-center">TODOS</h1>
 					<input
 						type="text"
 						className="form-control"
 						id="exampleInputEmail1"
 						aria-describedby="emailHelp"
-						placeholder="Enter email"
-						onChange={(e) => setTarea(e.target.value)}
+						placeholder="Escribe una tarea"
+						onChange={(e) =>
+							setTarea(e.target.value)
+						} /*aqui utilizamos el evento onChange y esa es su sintaxis*/
 						value={tarea}
 					/>
-					<ul>
-						{lista.map((tarea, index) => (
-							<li key={index}>{tarea}</li>
-							// se debe agregar  un icono que contenga un evento para eliminar un elemnto de la lista
-						))}
+					<ul className="list-group list-group-flush">
+						{lista.map(
+							(
+								item /*mapeamos la variable lista quien contiene el array
+						                       y dentro del <li> seguimos desarrollando el map */
+							) => (
+								<li
+									className=" lista list-group-item mt-1 mb-1 "
+									key={item.id}>
+									{" "}
+									{/*llamamos a item y le asignamos e id pero para porder llamarlos tenemos que asignar un key */}
+									{item.nombre}{" "}
+									{/* en medio de la etiqueta <li> y </li> llamos a itemy le asignamos nombre para poder renderizar*/}
+									<i
+										className="bi bi-x-lg position-absolute top-40 end-0"
+										onClick={() =>
+											borrarDatos(item.id)
+										}></i>
+									{/*dentro del icono creamos la funcion onClick
+									                                            para poder eliminar el item que se desee*/}
+								</li>
+							)
+						)}
 					</ul>
-					<button type="reset" class="btn btn-primary" value={lista}>
-						Submit
-					</button>
 				</div>
 			</form>
 		</div>
